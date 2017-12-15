2b5cdc1614252a72452e8596034d2a4eea6f503b
Phearun Rath <rathphearun168@gmail.com>
2017-12-15 19:55:16 +0700

    init

---
+++ oauth2-resource-server/src/main/java/com/phearun/configuration/ResourceServerConfiguration.java
@@ -0,0 +1,27 @@
+package com.phearun.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter{
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+			.and()
+				.sessionManagement()
+					.sessionCreationPolicy(SessionCreationPolicy.NEVER)
+			.and()
+				.authorizeRequests()
+					.anyRequest().authenticated()
+		;
+	}
+}
